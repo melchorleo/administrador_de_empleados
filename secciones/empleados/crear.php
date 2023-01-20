8bc3fab0e3f5fe562c855c817a7f3bea205d9723
aparicioo.leo@gmail.com <aparicioo.leo@gmail.com>
2023-01-20 13:53:07 -0600

    The routes were added, the header was separated with the footer

---
+++ secciones/empleados/crear.php
@@ -0,0 +1,5 @@
+<?php  include("../../templates/header.php");?>
+
+<h1>Aqui podras crear Empleados</h1>
+
+<?php include("../../templates/footer.php"); ?>
--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -1,5 +1,4 @@
 <?php include("../../templates/header.php"); ?>
-
-<h1>Listar Usuarios</h1>
+<h1> Listar puestos</h1>
 
 <?php include("../../templates/footer.php"); ?>
