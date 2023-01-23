265a08956ded87262f95f9e5e141b182d266dba6
aparicioo.leo@gmail.com <aparicioo.leo@gmail.com>
2023-01-23 17:55:13 -0600

    Se terminaron las interfaces

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,4 +1,37 @@
 <?php include("../../templates/header.php"); ?>
-<h1> Crear usuarios</h1>
+<br/>
+<div class="card">
+    <div class="card-header">
+        Datos del usuario
+    </div>
+    <div class="card-body">
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+              <label for="usuario  " class="form-label">Nombre del usuario:</label>
+              <input type="text"
+                class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Nombre del usuario">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password"
+                class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Escriba su contraseña">
+             
+            </div>
+
+            <div class="mb-3">
+              <label for="correo" class="form-label">Correo electronico</label>
+              <input type="email"
+                class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="Escriba su correo electronico">
+           
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+    </div>
+    <div class="card-footer text-muted">
+       
+    </div>
+</div>
 
 <?php include("../../templates/footer.php"); ?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,5 +1,40 @@
 <?php include("../../templates/header.php"); ?>
 
-<h1>Listar Usuarios</h1>
+<br/>
+
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar usuarios</a>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive-sm">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre del usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">Correo electronico</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">1</td>
+                <td>Leonardo Aparicio Melchor</td>
+                <td>*******</th>
+                <td>aparicioo.leo@gmail.com</td>
+                <td><input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar">
+
+                <input name="btneborrar" id="btnborrar" class="btn btn-danger" type="button" value="Eliminar"></td>
+            </tr>
+           
+        </tbody>
+    </table>
+</div>
+
+    </div>
+   
+</div>
 
 <?php include("../../templates/footer.php"); ?>
